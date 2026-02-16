c522f4391220c60800d20180eb287e3ad4371eb2
Josh Bloomstein <josh@Joshs-MacBook-Pro.local>
2026-02-15 17:13:25 -0800

    Electives 8

--- docs/amion/app.py
+++
@@ -1,9 +0,0 @@
-import importlib.util
-from pathlib import Path
-
-p = Path(__file__).parent / '02-15-26 Amion elective schedules 3.py'
-spec = importlib.util.spec_from_file_location('amion_app', str(p))
-mod = importlib.util.module_from_spec(spec)
-spec.loader.exec_module(mod)
-
-app = mod.app
